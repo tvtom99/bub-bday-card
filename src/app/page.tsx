fcda5838ae3f0dda0720f2b3cf38f0b50b3d9823
tvtom99 <tvtom99@yahoo.com.au>
2024-10-10 21:12:21 +0800

    actually fixed the eslint error by using shoddy coding practises

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import { Edu_VIC_WA_NT_Beginner, Qwitcher_Grypen } from 'next/font/google'
 
-//eslint-disable-next-line @typescript-eslint/no-unused-vars
 const eduFont = Edu_VIC_WA_NT_Beginner({ subsets: ['latin'] })
 const qwitch = Qwitcher_Grypen({ weight: ["400", "700"], subsets: ['latin'] })
+
+// Log the fonts in console so they actually are used lol
+console.log(eduFont, qwitch)
 
 export default function Home() {
   return (
